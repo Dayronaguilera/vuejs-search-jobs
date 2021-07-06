bb4a986abe3ec0083ce75fffdb586559151cb3da
Dayronaguilera <aguilera.dayron@gmail.com>
2021-07-06 12:11:35 +0200

    End milestone 1

--- js/script.js
+++ js/script.js
@@ -4,6 +4,51 @@ new Vue(
     {
         el: '#app',
         data: {
+            jobs: [
+                {
+                  id: 1,
+                  company: 'Perferendis',
+                  position: 'Developer',
+                  description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                  created_at: '5/22/2021',
+                  logo: 'logo.jpg',
+                  city: 'Roma',
+                  contract: 'Full Time'
+                },
+                {
+                    id: 2,
+                    company: 'volocom technology',
+                    position: 'Front End Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/24/2021',
+                    logo: 'logo.jpg',
+                    city: 'Roma',
+                    contract: 'Part Time'
+                },  
+                {
+                    id: 3,
+                    company: 'Umana spa',
+                    position: 'Junior developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/26/2021',
+                    logo: 'logo.jpg',
+                    city: 'Roma',
+                    contract: 'Full Time'
+                },  
+                {
+                    id: 4,
+                    company: 'Iselection',
+                    position: 'Programmatori java',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/30/2021',
+                    logo: 'logo.jpg',
+                    city: 'Roma',
+                    contract: 'Stage'
+                },  
+    
+            ],
+            starred: [1, 2, 3],
+            applied: [4, 5]    
         }
     }
  );
